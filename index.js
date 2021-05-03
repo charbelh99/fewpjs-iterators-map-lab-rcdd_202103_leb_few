63b15616131e63390f8074e1700754b54a19ea03
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-05-03 19:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials.map(cas => cas.split(" ").map(
-   word => word[0].toUpperCase() + + word.slice(1)).join(' '))
-}
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+   word => word[0].toUpperCase() + word.slice(1)
+).join(' ')
+
+)
